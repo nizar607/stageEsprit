891cfbc206979437bb08c24a08e25c26e8e2b4b4
MohamedMokhtarHadded <mohamedmokhtar.hadded@esprit.tn>
2023-08-07 15:15:30 +0100

    profile entreprise

--- src/app/layouts/company-layout/company-layout.component.ts
+++ src/app/layouts/company-layout/company-layout.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./company-layout.component.css']
 })
 export class CompanyLayoutComponent {
-
+  selectedLink = 0;
+  navLinks = [
+    "Find jobs",
+    "Company reviews",
+    "Find salaries"
+  ];
+  selectLink(index: number) {
+    this.selectedLink = index;
+  }
+  
+  getRange(size: number): number[] {
+    return Array(size).fill(0).map((value, index) => index);
+  }
 }
